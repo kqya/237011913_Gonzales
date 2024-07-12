474af71232c5cd887ea3b4f40c5bbb5c5d146b79
kqya <kyscape@gmail.com>
2024-07-12 18:41:45 +0800

    Batch script for launching websites, calculator, notepad, and initiates system shutdown

---
+++ BSCRIPT01.bat
@@ -0,0 +1,14 @@
+@echo off
+
+start "" "https://www.facebook.com/"
+start "" "https://x.com/home"
+start "" "https://www.youtube.com/"
+start "" "https://www.twitch.tv/"
+start "" "https://www.netflix.com/browse"
+
+start calc
+start notepad
+
+echo PC to shutdown.
+pause
+shutdown -s
